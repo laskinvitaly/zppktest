2a9ff386bf3f72a50350e43a49a85c8582c46586
laskinvitaly <laskinvitaly@gmail.com>
2024-03-20 23:12:43 +0300

    20.03.2024 собрал в кучу

--- app/Models/RequestRegister.php
+++ app/Models/RequestRegister.php
@@ -10,4 +10,31 @@ class RequestRegister extends Model
     use HasFactory;
     protected $table = 'request_registers';
     protected $guarded = false;
+
+    public function sername()
+    {
+        $mas = explode(" ", $this->fio);
+        if (isset($mas[0])){
+            $val = $mas[0];
+        } 
+        return $val;
+    }
+
+    public function name()
+    {
+        $mas = explode(" ", $this->fio);
+        if (isset($mas[1])){
+            $val = $mas[1];
+        } 
+        return $val;
+    }
+
+    public function parentname()
+    {
+        $mas = explode(" ", $this->fio);
+        if (isset($mas[2])){
+            $val = $mas[2];
+        } 
+        return $val;
+    }
 }
--- resources/views/admin/register/index.blade.php
+++ resources/views/admin/register/index.blade.php
@@ -31,34 +31,93 @@
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">
-                <table class="table table-hover text-nowrap">
-                  <thead>
-                    <tr>
-                      <th style="width: 20px;">Код</th>
-                      <th style="width: 150px;" class="text-left">ФИО</th>
-                      <th class="text-left">Телефон</th>
-                      <th style="width: 20px;">&nbsp;</th>
-                      <th style="width: 20px;">&nbsp;</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    @foreach ($requestRegisters as $requestRegister) 
-                    <tr>
-                      <td>{{ $requestRegister['id'] }}</td>
-                      <td>{{ $requestRegister['fio'] }}</td>  
-                      <td>{{ $requestRegister['phone'] }}</td>                     
-                      <td><a class="btn btn-block btn-success" href="">Одобрить</a></td>
-                      <td>
-                        <form action="" method="POST">
+                @foreach ($requestRegisters as $requestRegister) 
+                <div id="accordion">
+                  <div class="card card-primary">
+                    <div class="card-header">
+                      <h4 class="card-title w-100">
+                        <a class="d-block w-100" data-toggle="collapse" href="#collapseOne">
+                          {{ $requestRegister['fio'] }}
+                        </a>
+                      </h4>                     
+                    </div>
+                    <div id="collapseOne" class="collapse @if ($loop->first) show @endif" data-parent="#accordion">
+                      <div class="card-body">
+                        <form class="p-3" action="" method="POST">
                           @csrf
-                          @method('delete')
-                          <button type="submit" class="btn btn-block btn-danger"><i class="fas fa-trash-alt"></i></button>
+                            <div class="form-group">
+                              <label for="sername">Фамилия</label>
+                              <input type="text" name="sername" class="form-control @error('fio') is-invalid @enderror" placeholder="Введите фамилию" value="{{ $requestRegister->sername() }}" required autofocus>
+                              @error('sername')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                            </div>
+                            <div class="form-group">
+                              <label for="name">Имя</label>
+                              <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" placeholder="Введите имя" value="{{ $requestRegister->name() }}" required>
+                              @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                            </div>
+                            <div class="form-group">
+                              <label for="parentname">Отчество</label>
+                              <input type="text" name="parentname" class="form-control @error('parentname') is-invalid @enderror" placeholder="Введите отчество" value="{{ $requestRegister->parentname() }}" required>
+                              @error('parentname')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                            </div>
+                            <div class="form-group">
+                              <label for="phone">Номер телефона</label>
+                              <input type="text" name="phone" class="form-control input-phone @error('phone') is-invalid @enderror" placeholder="+7(000)-00-00" value="{{ $requestRegister['phone'] }}" required>
+                              @error('phone')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                            </div>
+                            <div class="form-group">
+                              <label for="password">Воспользуйтесь сгенерированным паролем или придумайте свой</label>
+                              <p class="text-danger">Не забудьте записать пароль. Пароли в БД хранятся в хешированном виде</p>
+                              <input type="text" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Введите пароль" value="{{ Str::password(8)}}" required>
+                              @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror                                
+                            </div>
+                                                        
+                            <div class="form-group">
+                              <label for="role"></label>
+                              <select name="role" class="custom-select rounded-0" >
+                                @foreach (config('settings.roles') as $key => $role)
+                                <option value="{{ $role['value'] }}" @if ($role['value']==2) selected="selected" @endif>{{ $role['text'] }}</option>
+                                @endforeach                                
+                              </select>
+                            </div>
+                            <div class="form-group">
+                              <label for="">Классный руководитель</label>
+                              <textarea class="form-control" rows="1" placeholder="Введите классного руководителя"> Иванова Мария Ивановна
+                              </textarea>
+                            </div>
+                            
+                            <button type="submit" class="btn btn-success">Одобрить</button> 
+                            <form action="" method="POST">
+                              @csrf
+                              @method('delete')
+                              <button type="submit" class="btn btn-danger">Удалить</button>
+                            </form>                   
                         </form>
-                      </td>                      
-                    </tr>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                     @endforeach 
-                  </tbody>
-                </table>
               </div>
               <!-- /.card-body -->
             </div>
